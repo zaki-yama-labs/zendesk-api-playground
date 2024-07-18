228ea9f798e58ecd600c6ebedeafdf13a67b2ff5
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2024-07-18 15:59:21 +0900

    support-api sample

---
+++ support-api/main.mjs
@@ -0,0 +1,13 @@
+import zendesk from 'node-zendesk';
+
+console.log(process.env.API_TOKEN);
+
+const client = zendesk.createClient({
+  username: process.env.USERNAME,
+  token: process.env.API_TOKEN,
+  subdomain: process.env.SUBDOMAIN
+});
+
+const tickets = await client.tickets.list();
+
+console.dir(tickets, {depth: null});
